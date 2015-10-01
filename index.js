d140bd69b6d59a4af5c43b0759896ff932bd86a1
Allan Crisostomo <kiiroifuriku@hotmail.com>
2015-10-02 02:25:11 +0800

    Update options

--- index.js
+++ index.js
@@ -10,12 +10,20 @@
         _parseArgs = function _parseArgs(args) {
             var _options = {
                 path: args[0],
-                goreDeg: args[1] || 5,
-                isTransparentGrayscale: false
+                goreDeg: args[1],
+                isTransparentGrayscale: args.indexOf('-G') !== -1 || args.indexOf('--grayscale-mask') !== -1
             };
 
             if(!_options.path) {
                 throw new Error('No PNG file specified.');
+            }
+
+            if(!_options.goreDeg) {
+                _options.goreDeg = 5;
+            }
+
+            if(isNaN(_options.goreDeg) || (360 % _options.goreDeg !== 0)) {
+                throw new Error('Gore degree must be a number that can divide 360 to an integer value.');
             }
 
             return _options;
